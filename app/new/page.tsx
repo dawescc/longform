0821fe1ef1258c6abde580b92977c506d04864f7
Ryan Dawes <dev@dawes.cc>
2024-02-18 14:11:34 -0500

    improved accessibility

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -12,6 +12,7 @@ export default function New() {
 				<div className='flex flex-col gap-5'>
 					<input
 						required
+						aria-label='Enter a Title'
 						name='title'
 						placeholder='Title'
 						className='font-semibold text-2xl bg-transparent focus:ring-0 h-full p-2 resize-none'></input>
@@ -22,6 +23,7 @@ export default function New() {
 			<div className='flex flex-col gap-5 flex-grow'>
 				<textarea
 					required
+					aria-label='Enter Post Content'
 					name='content'
 					placeholder='Content'
 					className='bg-transparent focus:ring-0 h-full p-2 resize-none font-medium leading-relaxed whitespace-pre-wrap'
@@ -30,12 +32,17 @@ export default function New() {
 
 			<div className='flex items-center justify-between'>
 				<Link
+					aria-label='Go Back'
 					href='/'
 					className='button default'>
 					<MoveLeft />
 					Back
 				</Link>
-				<button className='button action'>Post</button>
+				<button
+					aria-label='Submit New Post'
+					className='button action'>
+					Post
+				</button>
 			</div>
 		</form>
 	);
